8ca6e0b5e7dc6ccc447131e469570d238a7e227c
riki-nakayashiki <riki.nakayashiki@gmail.com>
2023-05-19 15:15:48 -0700

    Created files

---
+++ inc/Page.class.php
@@ -0,0 +1,70 @@
+<?php
+
+class Page
+{
+    /**
+     * @return string
+     */
+    public static function pageHeader(string $title = ""): string
+    {
+        $htmlHeader = '
+                        <!DOCTYPE html>
+                        <html lang="en">
+                        <head>
+                            <meta charset="UTF-8">
+                            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                            <title>' . $title . '</title>
+                            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
+                            <link rel="stylesheet" href="./css/style.css">
+                        </head>
+                        <body>
+                        ';
+        return $htmlHeader;
+    }
+
+    /**
+     * @return string
+     */
+    public static function pageBanner(): string
+    {
+        $htmlBanner = '
+        ';
+        return $htmlBanner;
+    }
+
+    /**
+     * @return string
+     */
+    public static function mainContent(): string
+    {
+        $mainContent = '<main>';
+        $mainContent .= '</main>';
+
+        return $mainContent;
+    }
+
+    /**
+     * @return string
+     */
+    public static function pageFooter(): string
+    {
+        $htmlFooter = '
+                <footer>
+                </footer>
+                ';
+        return $htmlFooter;
+    }
+
+    /**
+     * @return string
+     */
+    public static function pageEnd(): string
+    {
+        $htmlPageEnd = '
+                </body>
+                </html>';
+        return $htmlPageEnd;
+    }
+
+}
--- index.php
+++ index.php
@@ -1,1 +1,9 @@
 <?php
+
+require_once("inc/Page.class.php");
+
+echo Page::pageHeader("Final Project");
+echo Page::pageBanner();
+echo Page::mainContent();
+echo Page::pageFooter();
+echo Page::pageEnd();
