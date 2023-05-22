b20e0a1066d3bde883f2ab02cd137740cf1202ed
Monika Nemcova <moncanem@gmail.com>
2023-05-21 18:26:08 -0700

    footer function and scss done

--- inc/Page.class.php
+++ inc/Page.class.php
@@ -47,10 +47,66 @@ class Page
     /**
      * @return string
      */
+    public static function newsContent() : string {
+        $news = '
+
+        ';
+        return $news;
+    }
+
+    /**
+     * @return string
+     */
     public static function pageFooter(): string
     {
         $htmlFooter = '
-                <footer>
+                <footer class="footer">
+                    <section class="contact-follow">
+                        <article class="contact">
+                            <h4>Contact Us</h4>
+                            <p>Address:</p>
+                            <small>889 W Pender St, Vancouver, BC V6C 3B2</small>
+                            <p>Phone:</p>
+                            <a href="tel:123-456-7890">123-456-7890</a>
+                            <p>General Inquires:</p>
+                            <a href = "mailto: LibraWeb@email.com">LibraWeb@mail.com</a>
+                        </article>
+                        <article class="follow">
+                            <h4>Follow Us</h4>
+                            <ul>
+                                <li>
+                                    <a href="https://www.facebook.com/" target="_blank">
+                                        <i class="fa-brands fa-facebook-f"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://www.instagram.com/" target="_blank">
+                                        <i class="fa-brands fa-instagram"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://www.linkedin.com/" target="_blank">
+                                        <i class="fa-brands fa-linkedin-in"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://www.twitter.com/" target="_blank">
+                                        <i class="fa-brands fa-twitter"></i>
+                                    </a>
+                                </li>
+                            </ul>
+                        </article>
+                    </section>
+                    <section class="copyright">
+                        <p>
+                            &copy; copyright LibraWeb co. Designed by: Riki Nakayashiki, Junhyeok Kang and Monika Nemcova
+                        </p>
+                        <p>
+                        <a href="#">Terms of use</a> |
+                        <a href="#">Privacy policy</a> |
+                        <a href="#">Careers</a>
+                        </p>
+                    </section>
                 </footer>
                 ';
         return $htmlFooter;
